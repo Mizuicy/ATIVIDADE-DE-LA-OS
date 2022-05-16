ed2f017ae3ec8eb5b86cf23380b57f8c7a3af632
Mizuicy <102737167+Mizuicy@users.noreply.github.com>
2022-05-16 10:40:49 -0300

    Add files via upload

---
+++ atividadelaços.js
@@ -0,0 +1,31 @@
+//ATIVIDADE-1
+
+let X = 1 
+while (X <= 100 ) {
+    console.log(X)
+    X = X + 1
+}
+
+//ATIVIDADE-2
+
+/*let Y = 50 
+while (Y <= 100 ) {
+    console.log(Y)
+    Y = Y + 1
+}*/
+
+//ATIVIDADE-3
+
+/*let P = 10
+while (P >= 0){
+    console.log(P)
+    P--
+}*/
+
+//ATIVIDADE-4
+
+/*let H = 0 
+while (H <= 20){
+    console.log(H)
+    H = H + 2
+}*/
